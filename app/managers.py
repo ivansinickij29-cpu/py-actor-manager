c864067111f3abd760cce9e3db4bdee5240242a9
Ivan Synytskyi <ivansinickij29@icloud.com>
2026-05-10 18:05:16 +0200

    fix3

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,44 @@
 import sqlite3
+from models import Actor
+from typing import List
 
-from app.models import Actor
+
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self.connection = sqlite3.connect(self.db_name)
+        self.connection.execute(
+            f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT)")
 
 
-# add manager here
+    def create(self, first_name: str, last_name: str) -> None:
+        self.connection.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self.connection.commit()
+
+    def all(self) -> list[Actor]:
+        cursor = self.connection.execute(f"SELECT * FROM {self.table_name}")
+        rows = cursor.fetchall()
+        result = []
+        for row in rows:
+            actor = Actor(
+                id = row[0],
+                first_name = row[1],
+                last_name = row[2]
+            )
+            result.append(actor)
+        return result
+
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        self.connection.execute(f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?",
+                                (new_first_name, new_last_name, pk))
+        self.connection.commit()
+
+    def delete(self, pk: int) -> None:
+        self.connection.execute(f"DELETE FROM {self.table_name} WHERE id = ?", (pk,))
+        self.connection.commit()
+
